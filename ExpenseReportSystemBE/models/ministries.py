173639260fc157c706d42dc6e8933cc2c76bb11c
YeounJun Mac <pyj4104@hotmail.com>
2021-03-19 14:06:26 -0400

    Added deprecated column to Ministries, so that foreign key relations are not affected.

--- ExpenseReportSystemBE/models/ministries.py
+++ ExpenseReportSystemBE/models/ministries.py
@@ -22,17 +22,21 @@ class Ministries(Base):
 	__tablename__ = 'Ministries'
 	id = Column('id', Integer, primary_key=True, autoincrement=True)
 	ministry = Column('ministry', String(50), nullable=False, unique=True)
+	deprecated = Column('deprecated', Boolean, nullable=False, default=False)
 
 def initialize(dbsession):
 	items = dbsession.query(Ministries).all()
 	for item in items:
-		if item.ministry not in ministries:
-			print("Deleting {}".format(item.ministry))
-			dbsession.delete(item)
-			dbsession.flush()
+		print("Deleting {}".format(item.ministry))
+		item.deprecated = True
+	dbsession.flush()
 	for ministry in ministries:
 		isIn = dbsession.query(Ministries).filter_by(ministry=ministry).first()
 		if not isIn:
+			print("Adding {}".format(ministry))
 			entry = Ministries(ministry=ministry)
 			dbsession.add(entry)
-			print("Adding {}".format(ministry))
+		else:
+			if isIn.deprecated:
+				print("Reviving {}".format(ministry))
+				isIn.deprecated = False
